c804d3c7edcda8e024954cbcb556e6fe8e6de268
geraldkachi <fitzgeraldkachi@gmail.com>
2021-02-28 09:37:45 +0100

    post heade left icon

--- components/post/header/Header.js
+++ components/post/header/Header.js
@@ -1,26 +1,33 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import ProfilePicture from '../../profilepicture/ProfilePicture'
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import ProfilePicture from "../../profilepicture/ProfilePicture";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-const Header = ({imageUri, name}) => {
-    return (
-        <View style={styles.container}>
-            <ProfilePicture uri={imageUri} size={40} />
-            <Text style={styles.name}>{name}</Text>
-        </View>
-    )
-}
+const Header = ({ imageUri, name }) => {
+  return (
+    <View style={styles.container}>
+      <View style={{ flexDirection:"row" }}>
+        <ProfilePicture uri={imageUri} size={40} />
+        <Text style={styles.name}>{name}</Text>
+      </View>
+      <View>
+        <MaterialCommunityIcons name="dots-vertical" size={24} />
+      </View>
+    </View>
+  );
+};
 
-export default Header
+export default Header;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'row',
-    },
-    name: {
-        alignSelf:'center',
-        fontWeight: 'bold',
-        color: '#4f4f4f4'
-    }
-})
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  name: {
+    alignSelf: "center",
+    fontWeight: "bold",
+    color: "#4f4f4f4",
+  },
+});
